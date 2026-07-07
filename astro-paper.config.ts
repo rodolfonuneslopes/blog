f5adb55f69e652b87dba8faf5e5483c9b316b7d5
Rodolfo Lopes <rodolfo@nuneslopes.org>
2026-07-07 16:47:21 +0100

    Enable post editing feature in configuration

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -23,7 +23,8 @@ export default defineAstroPaperConfig({
     showArchives: true,
     showBackButton: true,
     editPost: {
-      enabled: false,
+      enabled: true,
+      url: "https://github.com/rodolfonuneslopes/blog/edit/main/",
     },
     search: "pagefind",
   },
